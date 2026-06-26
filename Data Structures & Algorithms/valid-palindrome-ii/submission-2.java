ef53c2d803c16b286d5ef46f3ecb1bbc997e7e76
prathamg07 <pratham.pg04@gmail.com>
2026-06-26 20:55:30 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int count = 0;
+        int end = s.length() - 1;
+
+        int i = 0;
+        while (i < s.length() / 2) {
+            if (s.charAt(i) != s.charAt(end)) {
+                int end1 = end - 1;
+                int j = i;
+
+                while (j < end1) {
+
+                    if (s.charAt(j) != s.charAt(end1)) {
+                        count++;
+                        break;
+                    }
+                    j++;
+                    end1--;
+                }
+
+                if (count == 1) {
+
+                    j = i + 1;
+                    while (j < end) {
+
+                        if (s.charAt(j) != s.charAt(end)) {
+                            return false;
+                        }
+
+                        j++;
+                        end--;
+                    }
+                } else {
+                    return true;
+                }
+            }
+
+            end--;
+            i++;
+        }
+
+        return true;
+    }
+}
