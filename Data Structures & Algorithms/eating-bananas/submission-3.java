b8a936c7f5d7db5984cbc8ceb56446fd8d00c16d
prathamg07 <pratham.pg04@gmail.com>
2026-08-02 02:16:20 +0530

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    
+    public boolean check (int k , int h , int [] piles){
+            int hours = 0;
+            for (int key : piles){
+                hours += (key + k - 1) / k;
+            }
+
+            if (hours <= h){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    public int minEatingSpeed(int[] piles, int h) {
+
+
+        int large=0;
+        for (int key : piles){
+            if (key > large){
+                large = key;
+            }
+        }
+
+        int low = 1;
+        int high = large;
+
+        while (low <= high){
+            int k = low + (high - low )/2;
+            if (check(k,h,piles)){
+                high = k-1;
+            }
+            else{
+                low = k+1;
+            }
+        }
+
+        return low;
+
+
+
+        
+    }
+}
