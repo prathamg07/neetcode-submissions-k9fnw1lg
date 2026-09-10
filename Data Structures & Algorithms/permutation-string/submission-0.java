23c648f150f15c4021f194654c8fb25e048c2b9b
prathamg07 <pratham.pg04@gmail.com>
2026-09-10 10:18:51 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        for (int i = 0; i < s1.length(); i++) {
+            count1[s1.charAt(i) - 'a']++;
+        }
+
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+
+            count2[s2.charAt(right) - 'a']++;
+
+            if (right - left + 1 > s1.length()) {
+                count2[s2.charAt(left) - 'a']--;
+                left++;
+            }
+
+            if (Arrays.equals(count1, count2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
